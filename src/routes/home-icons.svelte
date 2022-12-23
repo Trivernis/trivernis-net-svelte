78e6e88d274b27f984c2f58b65c4836f098b8b53
trivernis <trivernis@protonmail.com>
2022-12-23 20:09:44 +0100

    Initial rewrite of trivernis.net using svelte kit

---
+++ src/routes/home-icons.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Icon from "./icon.svelte";
+
+  import githubLogo from "$lib/assets/github-logo.png";
+  import discordLogo from "$lib/assets/discord-logo.png";
+  import mastodonLogo from "$lib/assets/mastodon-logo.png";
+</script>
+
+<div class="home-icons">
+  <Icon href="https://github.com/trivernis" src={githubLogo} alt="GitHub logo"/>
+  <Icon href="https://discord.gg/ZxzM2bTeXU" src={discordLogo} alt="Discord logo"/>
+  <Icon href="https://social.funkyfish.cool/@trivernis" src={mastodonLogo} alt="Mastodon logo"/>
+</div>
+
+<style lang="scss">
+
+.home-icons {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+}
+</style>
---
+++ src/routes/icon.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+export let src: string;
+export let alt: string;
+export let href: string;
+</script>
+
+<a {href}>
+    <img {src} {alt} />
+</a>
+
+<style lang="scss">
+  a {
+    padding: 15px;
+    img {
+      height: 50px;
+      width: auto;
+    }
+  }
+</style>
