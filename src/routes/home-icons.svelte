3a638291e062bb7c77970c17a2bf6b3356eaab25
trivernis <trivernis@protonmail.com>
2022-12-24 13:44:05 +0100

    Add matrix and gitea links and make discord and github smaller

--- src/routes/home-icons.svelte
+++ src/routes/home-icons.svelte
@@ -4,12 +4,18 @@
   import githubLogo from "$lib/assets/github-logo.png";
   import discordLogo from "$lib/assets/discord-logo.png";
   import mastodonLogo from "$lib/assets/mastodon-logo.png";
+  import giteaLogo from "$lib/assets/gitea-logo.png";
+  import matrixLogo from "$lib/assets/matrix-logo.png";
 </script>
 
 <div class="home-icons">
-  <Icon href="https://github.com/trivernis" src={githubLogo} alt="GitHub logo"/>
-  <Icon href="https://discord.gg/ZxzM2bTeXU" src={discordLogo} alt="Discord logo"/>
+  <Icon small={true} href="https://github.com/trivernis" src={githubLogo} alt="GitHub logo"/>
+  <Icon small={true} href="https://discord.gg/ZxzM2bTeXU" src={discordLogo} alt="Discord logo"/>
+</div>
+<div class="home-icons">
+  <Icon href="https://git.trivernis.net/Trivernis" src={giteaLogo} alt="Gitea logo"/>
   <Icon href="https://social.funkyfish.cool/@trivernis" src={mastodonLogo} alt="Mastodon logo"/>
+  <Icon href="https://matrix.to/#/@trivernis:trivernis.net" src={matrixLogo} alt="Matrix logo"/>
 </div>
 
 <style lang="scss">
--- src/routes/icon.svelte
+++ src/routes/icon.svelte
@@ -2,18 +2,26 @@
 export let src: string;
 export let alt: string;
 export let href: string;
+export let small = false;
 </script>
 
-<a {href}>
+<a {href} class="{small? 'small' : ''}">
     <img {src} {alt} />
 </a>
 
 <style lang="scss">
   a {
-    padding: 15px;
+    padding: 20px;
     img {
-      height: 50px;
+      height: 60px;
       width: auto;
+    }
+  }
+  a.small {
+    padding: 15px;
+
+    img {
+        height: 35px;
     }
   }
 </style>
