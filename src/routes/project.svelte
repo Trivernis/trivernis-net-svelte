1fa616a4d064301b1fd913cc564853f0eeab7941
trivernis <trivernis@protonmail.com>
2022-12-24 15:01:07 +0100

    Add a list of projects

---
+++ src/routes/project.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	export let name: string;
+	export let href: string;
+	export let description: string;
+</script>
+
+<div class="project">
+	<div>
+		<a {href}><h2>{name}</h2></a>
+	</div>
+	<p>{description}</p>
+</div>
+
+<style lang="scss">
+	.project {
+		display: flex;
+		flex-direction: column;
+		margin-top: 1em;
+		width: 100%;
+		a {
+			display: inline;
+			color: white;
+			h2 {
+				display: inline;
+			}
+		}
+	}
+</style>
